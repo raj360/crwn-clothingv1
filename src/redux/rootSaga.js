1503c9f53956fbe5043a2a6fbd6bedca80c26f4a
MUGABA <mugabamuha@gmail.com>
2019-11-05 12:58:33 +0300

    made a few changes

--- src/redux/rootSaga.js
+++
@@ -1,7 +0,0 @@
-import { all, call } from "redux-saga/effects";
-import { fetchCollectionsStart } from "./shop/shopSagas";
-import { userSagas } from "./user/userSaga";
-import { cartSagas } from "./cart/cartSagas";
-export default function* rootSage() {
-  yield all([call(fetchCollectionsStart), call(userSagas), call(cartSagas)]);
-}
